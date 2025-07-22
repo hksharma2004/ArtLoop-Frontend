67e84be9db088d482f3d19e1704081eecc007257
Harsh <141849926+hksharma2004@users.noreply.github.com>
2025-07-22 10:07:26 +0530

    Update retro-grid.tsx

--- src/components/magicui/retro-grid.tsx
+++ src/components/magicui/retro-grid.tsx
@@ -1,34 +1,17 @@
 import { cn } from "@/lib/utils";
 
 interface RetroGridProps extends React.HTMLAttributes<HTMLDivElement> {
-  /**
-   * Additional CSS classes to apply to the grid container
-   */
+  
   className?: string;
-  /**
-   * Rotation angle of the grid in degrees
-   * @default 65
-   */
+  
   angle?: number;
-  /**
-   * Grid cell size in pixels
-   * @default 60
-   */
+  
   cellSize?: number;
-  /**
-   * Grid opacity value between 0 and 1
-   * @default 0.5
-   */
+  
   opacity?: number;
-  /**
-   * Grid line color in light mode
-   * @default "gray"
-   */
+  
   lightLineColor?: string;
-  /**
-   * Grid line color in dark mode
-   * @default "gray"
-   */
+  
   darkLineColor?: string;
 }
 
